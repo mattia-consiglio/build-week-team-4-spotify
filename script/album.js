014c055d3f88b7e002c51ac7e0a9281692f452cd
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2024-01-26 09:21:31 +0100

    updated player and smll changes to css

--- script/album.js
+++ script/album.js
@@ -1,54 +1,46 @@
 window.onload = function () {
-  const albumTitle = document.getElementById("albumTitle");
-  const albumCover = document.getElementById("albumCover");
-  const albumArtist = document.getElementById("albumArtist");
-  const albumTracksNumber = document.getElementById("albumTracksNumber");
-  const releaseDate = document.getElementById("releaseDate");
-  const albumDuration = document.getElementById("albumDuration");
-  const artistImg = document.getElementById("artistImg");
+	const albumTitle = document.getElementById('albumTitle')
+	const albumCover = document.getElementById('albumCover')
+	const albumArtist = document.getElementById('albumArtist')
+	const albumTracksNumber = document.getElementById('albumTracksNumber')
+	const releaseDate = document.getElementById('releaseDate')
+	const albumDuration = document.getElementById('albumDuration')
+	const artistImg = document.getElementById('artistImg')
 
-  const id = new URLSearchParams(window.location.search).get("id");
+	const id = new URLSearchParams(window.location.search).get('id')
 
-  if (!id) {
-    window.location = "./404.html";
-    return;
-  }
+	if (!id) {
+		window.location = './404.html'
+		return
+	}
 
-  const durationToString = (duration, format = 1) => {
-    const hours = Math.floor(duration / 3600);
-    const minutes = Math.floor((duration - hours * 3600) / 60);
-    const seconds = Math.floor(duration - hours * 3600 - minutes * 60);
-    let hoursString;
-    let minutesString;
-    if (format === 1) {
-      hoursString =
-        hours > 1 ? hours + " ore " : hours === 1 ? hours + " ora " : "";
-      minutesString =
-        minutes > 1
-          ? minutes + " minuti"
-          : minutes === 1
-          ? minutes + " minuto"
-          : "";
-      return `${hoursString}${minutesString}`;
-    }
-    if (format === 2) {
-      hoursString = hours ? hours + ":" : "";
-      minutesString = minutes + ":";
+	const durationToString = (duration, format = 1) => {
+		const hours = Math.floor(duration / 3600)
+		const minutes = Math.floor((duration - hours * 3600) / 60)
+		const seconds = Math.floor(duration - hours * 3600 - minutes * 60)
+		let hoursString
+		let minutesString
+		if (format === 1) {
+			hoursString = hours > 1 ? hours + ' ore ' : hours === 1 ? hours + ' ora ' : ''
+			minutesString = minutes > 1 ? minutes + ' minuti' : minutes === 1 ? minutes + ' minuto' : ''
+			return `${hoursString}${minutesString}`
+		}
+		if (format === 2) {
+			hoursString = hours ? hours + ':' : ''
+			minutesString = minutes + ':'
 
-      return `${hoursString}${minutesString}${seconds
-        .toString()
-        .padStart(2, "0")}`;
-    }
-  };
+			return `${hoursString}${minutesString}${seconds.toString().padStart(2, '0')}`
+		}
+	}
 
-  const createTracksRows = (tracks) => {
-    const container = document.getElementById("tracksContainer");
-    container.innerHTML = "";
-    tracks.forEach((track, i) => {
-      const row = document.createElement("div");
-      row.classList.add("table-row");
-      row.setAttribute("role", "row");
-      row.innerHTML = `
+	const createTracksRows = tracks => {
+		const container = document.getElementById('tracksContainer')
+		container.innerHTML = ''
+		tracks.forEach((track, i) => {
+			const row = document.createElement('div')
+			row.classList.add('table-row')
+			row.setAttribute('role', 'row')
+			row.innerHTML = `
 		<div class="table-cell" role="gridcell" aria-colindex="1">
 			<button class="btn bg-transparent"><span class="icon-medium">
 				<svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24"
@@ -63,8 +55,8 @@ window.onload = function () {
 												<div class="" dir="auto" data-encore-id="text">${track.title}</div>
 											</a><span class="" data-encore-id="text"><span><a class="text-decoration-none text-body-secondary"
 														dir="auto" href="./artist.html?id=${track.artist.id}" tabindex="-1">${
-        track.artist.name
-      }</a></span></span></div>
+				track.artist.name
+			}</a></span></span></div>
 									</div>
 									<div class="table-cell justify-content-around" role="gridcell" aria-colindex="3">
 										<button class="btn bg-transparent d-flex border-0 py-0 opacity-0" aria-checked="false"
@@ -107,35 +99,34 @@ window.onload = function () {
                     
 
                       
-		`;
-      container.appendChild(row);
-    });
-  };
+		`
+			container.appendChild(row)
+		})
+	}
 
-  const fillInfo = (data) => {
-    albumTitle.innerText = data.title;
-    albumCover.src = data.cover_xl;
-    albumCover.alt = data.title;
-    albumArtist.innerText = data.artist.name;
-    albumTracksNumber.innerText = data.nb_tracks + " brani";
-    releaseDate.innerText = data.release_date.split("-")[0];
-    artistImg.src = data.artist.picture_small;
-    artistImg.alt = data.artist.name;
-    albumDuration.innerText = durationToString(data.duration, 1);
-    createTracksRows(data.tracks.data);
-    albumCover.addEventListener("load", () => {
-      setTimeout(() => {
-        const avgColor = getMostRecurrentColor(albumCover);
-        Array.from(document.getElementsByClassName("customBg")).forEach(
-          (el) => {
-            // el.style.backgroundColor = `rgb(${avgColor.r}, ${avgColor.g}, ${avgColor.b})`
-            el.style.backgroundColor = avgColor;
-          }
-        );
-      }, 100);
-    });
-    removePlaceholders();
-  };
+	const fillInfo = data => {
+		albumTitle.innerText = data.title
+		albumCover.src = data.cover_xl
+		albumCover.alt = data.title
+		albumArtist.innerText = data.artist.name
+		albumTracksNumber.innerText = data.nb_tracks + ' brani'
+		releaseDate.innerText = data.release_date.split('-')[0]
+		artistImg.src = data.artist.picture_small
+		artistImg.alt = data.artist.name
+		albumDuration.innerText = durationToString(data.duration, 1)
+		createTracksRows(data.tracks.data)
+		albumCover.addEventListener('load', () => {
+			setTimeout(() => {
+				const avgColor = getMostRecurrentColor(albumCover)
+				Array.from(document.getElementsByClassName('customBg')).forEach(el => {
+					// el.style.backgroundColor = `rgb(${avgColor.r}, ${avgColor.g}, ${avgColor.b})`
+					el.style.backgroundColor = avgColor
+				})
+			}, 100)
+		})
+		removePlaceholders()
+		generateTracks(data.tracks.data, 'album')
+	}
 
-  api("album/" + id, fillInfo);
-};
+	api('album/' + id, fillInfo)
+}
--- script/player.js
+++ script/player.js
@@ -1,179 +1,154 @@
 // Ottieni l'elemento audio e gli elementi personalizzati
-const audioPlayer = document.getElementById("audio-player");
-const currentTimeDisplay = document.getElementById("current-time");
-const seekBar = document.getElementById("seek-bar");
-const durationDisplay = document.getElementById("duration");
+const audioPlayer = document.getElementById('audio-player')
+const currentTimeDisplay = document.getElementById('current-time')
+const progressInput = document.getElementById('seek-bar')
+const durationDisplay = document.getElementById('duration')
+const audioSource = document.getElementById('audio-source')
+const playPauseButton = document.getElementById('player-play-pause-button')
+const prevButton = document.getElementById('player-prev')
+const nextButton = document.getElementById('player-next')
+const volumeInput = document.getElementById('volume-input')
+const volumeButton = document.getElementById('volume-button')
+const coverImage = document.getElementById('player-cover-image')
+const title = document.getElementById('player-title')
+const artist = document.getElementById('player-artist')
 
-// Imposta la durata totale dell'audio quando è pronto
-audioPlayer.addEventListener("loadedmetadata", function () {
-  const duration = audioPlayer.duration;
-  durationDisplay.textContent = formatTime(duration);
-  seekBar.max = duration;
-});
+let audioLoaded = false
+let isChangingTime = false
+let minVolume = 0.25
+let volume = 0.6
 
-// Aggiorna la barra dei secondi e il tempo corrente durante la riproduzione
-audioPlayer.addEventListener("timeupdate", function () {
-  const currentTime = audioPlayer.currentTime;
-  currentTimeDisplay.textContent = formatTime(currentTime);
-  seekBar.value = currentTime;
-});
+let currentTrack = 0
+const tracks = []
+const quee = []
+
+const generateTracks = data => {
+	data.forEach(track => {
+		const trackObject = {
+			id: track.id,
+			title: track.title,
+			preview: track.link,
+			artist: {
+				id: track.artist.id,
+				name: track.artist.name,
+			},
+			cover: track.album.cover_small,
+		}
+		tracks.push(trackObject)
+	})
+	console.log(tracks)
+}
 
 // Formatta il tempo nel formato mm:ss
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+const formatTime = time => {
+	const minutes = Math.floor(time / 60)
+	const seconds = Math.ceil(time % 60)
+	return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
 }
-// const sourceAudio = document.getElementById("audio-source");
 
-const changeAudio = function (src) {
-  const sourceAudio = document.getElementById("audio-source");
-  sourceAudio.src = src;
-  audioPlayer.load();
-};
-changeAudio(
-  "https://cdns-preview-b.dzcdn.net/stream/c-bcf686b9b7b146a3ce3d160cbfa2d1b5-7.mp3"
-);
+// Imposta la durata totale dell'audio quando è pronto
+audioPlayer.addEventListener('loadeddata', function () {
+	console.log('Audio source loded', audioPlayer)
+	const duration = audioPlayer.duration
+	durationDisplay.textContent = formatTime(duration)
+	progressInput.max = duration
+	audioLoaded = true
+})
 
-// const changeAudio = function () {
-//   fetch("https://striveschool-api.herokuapp.com/api/deezer/album/75621062")
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(
-//           `Errore nella risposta della richiesta: ${response.statusText}`
-//         );
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       const randomIndex = Math.floor(Math.random() * data.length);
-//       const randomUrl = data[randomIndex];
-
-//       sourceAudio.src = randomUrl;
-//       audioPlayer.load();
-//     })
-//     .catch((error) => {
-//       console.error("Errore durante il recupero dei dati:", error);
-//     });
-// };
-
-// window.onload = changeAudio;
-
-// const audioPlayer = document.getElementById("audio-player");
-const sourceAudio = document.getElementById("audio-source");
-
-// const changeAudio = function () {
-//   fetch(
-//     "https://striveschool-api.herokuapp.com/api/deezer/artist/412/top?limit=50"
-//   )
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(
-//           `Errore nella risposta della richiesta: ${response.statusText}`
-//         );
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       // Assicurati che l'oggetto restituito abbia una proprietà "link"
-//       if (!data || !data.link) {
-//         throw new Error("L'oggetto restituito non ha una proprietà 'link'.");
-//       }
-
-//       const randomUrl = data.link; // Modifica qui in base alla tua struttura dati
-
-//       sourceAudio.src = randomUrl;
-//       audioPlayer.load();
-//     })
-//     .catch((error) => {
-//       console.error("Errore durante il recupero dei dati:", error);
-//     });
-// };
-
-// window.onload = changeAudio;
-
-$(document).ready(function () {
-  // Seleziona i bottoni e l'elemento audio
-  const playButton = $(".play");
-  const audio = $("#audio-player")[0]; // [0] è necessario per ottenere l'elemento JavaScript dall'oggetto jQuery
-
-  // Gestisci il clic sul pulsante di riproduzione/pausa
-  playButton.on("click", function () {
-    if (audio.paused) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  });
-
-  // Aggiorna il pulsante di riproduzione quando cambia lo stato dell'audio
-  audio.addEventListener("play", function () {
-    playButton
-      .find("svg.play path")
-      .attr(
-        "d",
-        "M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"
-      );
-  });
-
-  audio.addEventListener("pause", function () {
-    playButton.find("svg.play path").attr("d", "M0 0h6v16H0zm8 0h6v16H8z");
-  });
-
-  // Aggiorna la barra di avanzamento del tempo
-  audio.addEventListener("timeupdate", function () {
-    const currentTime = audio.currentTime;
-    const duration = audio.duration;
-
-    // Aggiorna la visualizzazione del tempo corrente
-    $("#current-time").text(formatTime(currentTime));
-
-    // Aggiorna la barra di avanzamento
-    const progress = (currentTime / duration) * 100;
-    $("#seek-bar").val(progress);
-
-    // Aggiorna la visualizzazione della durata totale
-    $("#duration").text(formatTime(duration));
-  });
-
-  // Gestisci il trascinamento della barra di avanzamento
-  $("#seek-bar").on("input", function () {
-    const progress = $(this).val();
-    const duration = audio.duration;
-    const currentTime = (progress / 100) * duration;
-
-    // Imposta il tempo corrente dell'audio
-    audio.currentTime = currentTime;
-  });
-
-  // Funzione per formattare il tempo nel formato mm:ss
-  function formatTime(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60);
-    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  }
-});
-
-
-
-
-
-
-const updateRangeInput = input => {
-	const max = input.max
-	const min = input.min
-	const value = input.value
-	const step = input.step
-	let perc = 0
-	if (value > min) {
-		perc = (value - min) / (max - min)
+// Aggiorna la barra dei secondi e il tempo corrente durante la riproduzione
+audioPlayer.addEventListener('timeupdate', function () {
+	const currentTime = audioPlayer.currentTime
+	currentTimeDisplay.textContent = formatTime(currentTime)
+	if (!isChangingTime) {
+		progressInput.value = currentTime
+		updateRangeInput(progressInput)
 	}
-	input.style.setProperty('--value', perc * 100 + '%')
+})
+
+//cambia la sorgente audio
+const playTrack = function (track) {
+	audioLoaded = false
+	audioSource.src = track.preview
+	audioPlayer.load()
+	coverImage.src = track.cover
+	coverImage.alt = track.title
+	title.innerText = track.title
+	artist.innerText = track.artist.name
+	artist.href = './artist.html?artistId=' + track.artist.id
 }
 
-document.querySelectorAll("input[type=range]").forEach((input) => {
-  updateRangeInput(input)
-  input.addEventListener('input', ()=> {
-    updateRangeInput(input)
-  } )
+playPauseButton.addEventListener('click', () => {
+	const span = playPauseButton.querySelector('span')
+	if (playPauseButton.ariaLabel === 'Play' && audioLoaded && audioPlayer.paused) {
+		playPauseButton.ariaLabel = 'Pause'
+		span.innerHTML = '<i class="si si-pause"></i>'
+		audioPlayer.play()
+	} else {
+		playPauseButton.ariaLabel = 'Play'
+		span.innerHTML = '<i class="si si-play"></i>'
+		audioPlayer.pause()
+	}
+})
+
+const setVolume = vol => {
+	audioPlayer.volume = vol
+	volume = vol
+	volumeInput.value = vol
+	console.log(vol)
+	const icon = volumeButton.querySelector('span')
+	updateRangeInput(volumeInput)
+
+	if (vol > 0.66) {
+		icon.innerHTML = '<i class="si si-volume-high"></i>'
+	} else if (vol > 0.33) {
+		icon.innerHTML = '<i class="si si-volume-medium"></i>'
+	} else if (vol > 0) {
+		icon.innerHTML = '<i class="si si-volume-low"></i>'
+	} else {
+		icon.innerHTML = '<i class="si si-volume-mute"></i>'
+	}
+}
+
+// Inizializza il player
+const initializePlayer = () => {
+	setVolume(volumeInput.value)
+	playTrack({
+		id: 568120922,
+		preview: 'https://cdns-preview-b.dzcdn.net/stream/c-bcf686b9b7b146a3ce3d160cbfa2d1b5-7.mp3',
+		cover:
+			'https://e-cdns-images.dzcdn.net/images/cover/8b8fc5d117f9357b79f0a0a410a170e8/56x56-000000-80-0-0.jpg',
+		title: '20th Century Fox Fanfare',
+		artist: {
+			id: 412,
+			name: 'Queen',
+		},
+	})
+}
+
+initializePlayer()
+
+progressInput.addEventListener('input', () => {
+	isChangingTime = true
+	audioPlayer.currentTime = progressInput.value
+	isChangingTime = false
+})
+
+volumeInput.addEventListener('input', () => {
+	const volume = parseFloat(volumeInput.value)
+	setVolume(volume)
+})
+
+volumeButton.addEventListener('click', () => {
+	if (volume === 0) {
+		setVolume(minVolume)
+	} else {
+		setVolume(0)
+	}
+})
+
+document.querySelectorAll('input[type=range]').forEach(input => {
+	updateRangeInput(input)
+	input.addEventListener('input', () => {
+		updateRangeInput(input)
+	})
 })
